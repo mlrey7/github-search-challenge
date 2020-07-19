e5efb92f45b81974c3e62a87ef83c6c906d63012
Matthew Lemuel M Rey <matthewlemuelr@gmail.com>
2020-07-19 18:44:49 +0800

    config: Added webpack config for progressive-img static files

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "progressive-img": ["src", "placeholder", "fallback"],
+        };
+        return options;
+      });
+  },
+};
